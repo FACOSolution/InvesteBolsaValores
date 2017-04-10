322ef221ec29bf7a58aec8ca0f533f270e686821
Edson <moitaneto@hotmail.com>
2017-04-09 21:18:16 -0300

    View mostrar_grafico implementada corretamente, montando a carteira com os dados do formulário.Testes de passagem de parametros pro template.

--- site_bolsa/site_functions/views.py
+++ site_bolsa/site_functions/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-
+from .objects.Carteira import Carteira
 
 def index(request):
-    return render(request, 'site_functions/index.html')
+    teste = 'Fala vagabundo'
+    return render(request, 'site_functions/index.html', {'teste': teste,})
 
 def mostrar_grafico(request):
-    print('passou aqui')
-    return render(request, 'site_functions/grafico.html')
+    if request.method == 'POST':
+        tamanho = int(request.POST.get('tamanho', False))
+        print(type(tamanho))
+        juros = request.POST.get('juros', False)
+        mercado = request.POST.get('mercado', False)
+        metodo = request.POST.get('metodo', False)
+        c = Carteira(tamanho, mercado, metodo)
+
+        print('passou aqui')
+        return render(request, 'site_functions/grafico.html', {'carteira': c})
+    else:
+        return render(request, 'site_functions/index.html')
